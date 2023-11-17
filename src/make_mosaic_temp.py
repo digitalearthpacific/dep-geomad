e5e31707208639bb8a69418c8dd8c7fa6efd7356
Alex Leith <alex@auspatious.com>
2023-11-17 06:25:46 +0000

    make it work

--- src/make_mosaic_temp.py
+++ src/make_mosaic_temp.py
@@ -15,12 +15,17 @@ base_url = "https://deppcpublicstorage.blob.core.windows.net/output/{key}"
 items = [Item.from_file(base_url.format(key=d)) for d in docs]
 
 data = load(items, chunks={}, resolution=30, crs="epsg:3832")
-print(data)
+print("Prepared data")
 
 rgba = data.odc.to_rgba(vmin=7000, vmax=12000)
+print("Converted to RGBA")
 
 # Writing to Azure
+print("Writing to Azure...")
 data = rgba.squeeze().odc.to_cog()
-blob_thing = client.get_blob_client("output/dep_ls_geomad/0-0-0/fiji_rgb_30m.tif")
+output_file = "dep_ls_geomad/0-0-1/fiji_rgb_30m.tif"
+blob_thing = client.get_blob_client(output_file)
+blob_thing.upload_blob(data, overwrite=True)
 
+print(f"{base_url}/{output_file}")
 print("I think it worked!!")
