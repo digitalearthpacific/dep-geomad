abb9c520becb0554087c08c897f5c08204b9c776
Alex <alexgleith@gmail.com>
2024-11-13 16:35:13 +1100

    Update run_task to not use postprocessor

--- src/run_task.py
+++ src/run_task.py
@@ -212,6 +212,7 @@ def main(
                     processor=processor,
                     writer=writer,
                     logger=log,
+                    post_processor=None
                 ).run()
     except EmptyCollectionError:
         log.info("No items found for this tile")
